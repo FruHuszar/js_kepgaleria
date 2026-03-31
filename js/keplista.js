7399d449344ffa7de2cc35c902a4be94e7f7e242
Fru <fruzsinaanna.huszar@gmail.com>
2026-03-31 08:47:29 +0200

    Update keplista.js

--- js/keplista.js
+++ js/keplista.js
@@ -1,37 +1,37 @@
 export const keplista = [
 {
     index: 0,
-    src: "Kepek/Kep1.jpg",
+    src: "kepek/Kep1.jpg",
     cim: "Tortacsoport",
     leiras: "4 torta: gyümölcsös, trópusi..."
 },
 {
     index: 1,
-    src: "Kepek/Kep2.jpg",
+    src: "kepek/Kep2.jpg",
     cim: "Citromos torta",
     leiras: "Citromkarikákkal és levelekkel"
 },
 {
     index: 2,
-    src: "Kepek/Kep3.jpg",
+    src: "kepek/Kep3.jpg",
     cim: "Epertorta",
     leiras: "Egész eperrel és százszorszéppel"
 },
 {
     index: 3,
-    src: "Kepek/Kep4.jpg",
+    src: "kepek/Kep4.jpg",
     cim: "Citrom Jelly torta",
     leiras: "Citromos zselével"
 },
 {
     index: 4,
-    src: "Kepek/Kep5.jpg",
+    src: "kepek/Kep5.jpg",
     cim: "Epres Csokitorta",
     leiras: "3 rétegű piskóta"
 },
 {
     index: 5,
-    src: "Kepek/Kep6.jpg",
+    src: "kepek/Kep6.jpg",
     cim: "Zselétorta",
     leiras: "Sárga virágokkal"
 },
